a180fd1e545116d93bbe4ad5a3987edb195156b3
Thibaud <t.galland@bouygues-construction.com>
2020-11-18 18:41:01 +0100

    init

---
+++ ForegeViewer.Net/wwwroot/ForgeViewer.js
@@ -0,0 +1,5 @@
+﻿import 'https://developer.api.autodesk.com/modelderivative/v2/viewers/7.*/viewer3D.js';
+
+export function GetViewer(options){
+    Autodesk.Viewing.Initializer(options, initializerHelper.invokeMethodAsync("InitializerCallback"));
+}
