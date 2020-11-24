eec586243c541fede029edd02ec8c1cba2f99e58
Thibaud <t.galland@bouygues-construction.com>
2020-11-24 11:31:29 +0100

    added firsts methods

--- ForgeViewer.NET/Viewing/Model.cs
+++ ForgeViewer.NET/Viewing/Model.cs
@@ -1,8 +1,10 @@
+using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
 using Microsoft.JSInterop;
 
 namespace ForgeViewer.NET.Viewing
 {
+    [SuppressMessage("ReSharper", "UnusedMember.Global")]
     public class Model
     {
         #region Ctor
@@ -31,10 +33,50 @@ namespace ForgeViewer.NET.Viewing
         {
             return await JsModel.InvokeAsync<bool>("is3d");
         }
-
+        public async Task<bool> IsOtg()
+        {
+            return await JsModel.InvokeAsync<bool>("isOTG");
+        }
+        public async Task<bool> IsSvf2()
+        {
+            return await JsModel.InvokeAsync<bool>("isSVF2");
+        }
         public async Task<bool> IsPdf(bool onlyPdfSource = true)
         {
             return await JsModel.InvokeAsync<bool>("isPdf",onlyPdfSource);
+        }
+        public async Task<bool> IsLeaflet()
+        {
+            return await JsModel.InvokeAsync<bool>("isLeaflet");
+        }
+        public async Task<bool> IsAec()
+        {
+            return await JsModel.InvokeAsync<bool>("isAEC");
+        }
+        public async Task<bool> IsPageCoordinates()
+        {
+            return await JsModel.InvokeAsync<bool>("isPageCoordinates");
+        }
+        public async Task<bool> IsSceneBuilder()
+        {
+            return await JsModel.InvokeAsync<bool>("isSceneBuilder");
+        }
+        public async Task GetData()
+        {
+            await JsModel.InvokeVoidAsync("getData");
+        }
+        public async Task<BubbleNode> GetDocumentNode()
+        {
+            var reference =  await JsModel.InvokeAsync<IJSObjectReference>("getDocumentNode");
+            return BubbleNode.Create(reference);
+        }
+        public async Task<IJSObjectReference> GetRoot() //TODO Get kind of item
+        {
+            return await JsModel.InvokeAsync<IJSObjectReference>("getRoot");
+        }
+        public async Task<double> GetRootId()
+        {
+            return await JsModel.InvokeAsync<double>("getRootId");
         }
 
         #endregion
